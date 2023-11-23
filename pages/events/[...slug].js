2c9ffe03127dd38cf3f7e8affe3a49ae894fe389
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2023-11-23 17:19:38 +0530

    add styled components to error messages

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,7 @@
 import EventList from "@/components/events/event-list";
+import Button from "@/components/ui/button";
+import ErrorAlert from "@/components/ui/error-alert";
+import ResultsTitle from "@/components/ui/results-title";
 import { getFilteredEvents } from "@/dummy-data";
 import { useRouter } from "next/router";
 
@@ -6,19 +9,38 @@ const EventFilterPage = () => {
   const router = useRouter();
   const date = router.query.slug;
   if (!date || date.length !== 2) {
-    return <p className="center">Loading</p>;
+    return <p className>Loading</p>;
   }
   const year = +date[0];
   const month = +date[1];
   if (isNaN(year) || isNaN(month)) {
-    return <p>Invalid path.</p>;
+    return (
+      <>
+        <ErrorAlert>
+          <p>Invalid path.</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All events</Button>
+        </div>
+      </>
+    );
   }
   const events = getFilteredEvents({ year: year, month: month });
   if (!events || events.length === 0) {
-    return <p>No events found for chosen period.</p>;
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found for chosen period.</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All events</Button>
+        </div>
+      </>
+    );
   }
   return (
     <div>
+      <ResultsTitle date={new Date(year, month)} />
       <EventList eventlist={events} />;
     </div>
   );
