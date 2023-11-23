30bfc9b6cf557790de5343f40fd8c6d0c2141f03
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2023-11-23 17:19:37 +0530

    added all events home page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,9 @@
+import EventList from "@/components/events/event-list";
+import { getAllEvents } from "@/dummy-data";
 import React from "react";
 
 const EventsHomePage = () => {
-  return <div>EventHomePage</div>;
+  return <EventList eventlist={getAllEvents()} />;
 };
 
 export default EventsHomePage;
