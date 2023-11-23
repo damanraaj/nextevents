f8a8e679fd6e5c9cb88c10a9e9c1e8cb53cbb63f
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2023-11-23 17:19:37 +0530

    added event detail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,29 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { getEventById } from "@/dummy-data";
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
 
 const EventPage = () => {
   const router = useRouter();
-  return <div>EventPage: {router.query.eventId}</div>;
+  const eventId = router.query.eventId;
+  const eventData = getEventById(eventId);
+  if (!eventData) {
+    return <p>No event found!</p>;
+  }
+  return (
+    <>
+      <EventSummary title={eventData.title} />
+      <EventLogistics
+        date={eventData.date}
+        address={eventData.location}
+        image={eventData.image}
+        imageAlt={eventData.title}
+      />
+      <EventContent>{eventData.description}</EventContent>
+    </>
+  );
 };
 
 export default EventPage;
