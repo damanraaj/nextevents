419d021640305e6dce609d9a22cc07b21fd66c99
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2023-11-23 17:19:37 +0530

    added icons to event list page

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,7 +1,9 @@
 import React from "react";
-import Link from "next/link";
 import classes from "./event-item.module.css";
 import Button from "../ui/button";
+import AddressIcon from "../icons/address-icon";
+import DateIcon from "../icons/date-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 const EventItem = (props) => {
   const { id, title, date, image, location } = props.event;
@@ -14,14 +16,21 @@ const EventItem = (props) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{eventDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{location.replace(", ", "\n")}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore event</Button>
+          <Button link={exploreLink}>
+            Explore event
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
